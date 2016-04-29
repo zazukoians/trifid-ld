18e61532a87f7f715a7270f1cbc87d94bb425bb4
giuseppefutia <giuseppe.futia@gmail.com>
2016-04-29 17:21:53 +0200

    Integrated dbpedia with sameas

--- data/public/js/dbpedia.js
+++ data/public/js/dbpedia.js
@@ -1,6 +1,6 @@
 $.ajax({
   type: "GET",
-  url: "http://localhost:8080/dbpedia-query?q=Turin",
+  url: "http://localhost:8080/dbpedia-query?q=Sheldon_Cooper",
   cache: false,
   success: function(html){
     $("#data").append(document.createTextNode(html));
