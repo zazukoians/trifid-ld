d349d88eb030e9595e3f8246d7ec8a9badd238bd
alemela <alessiom92@gmail.com>
2016-04-30 12:35:05 +0200

    made the dbpedia enricher active only when a sameAs to it is present

--- data/public/js/dbpedia.js
+++ data/public/js/dbpedia.js
@@ -4,9 +4,7 @@ function dbpediaGet (entity, callback) {
       url: "http://localhost:8080/dbpedia-query?q=" + entity,
       cache: false,
       success: function(html){
-        alert("fatto");
         $("#data").append(document.createTextNode(html));
-        alert("scritto");
         callback();
       },
       error: function(XMLHttpRequest, textStatus, errorThrown) { 
